fb068f241ef3998f573ff6f338df1eca1b8ef298
caopengwei <2069420645@qq.com>
2019-11-02 15:57:51 +0800

    jiuwanli666
    ant-design-vue
    相关内容测试

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+    <a-collapse v-model="activeKey">
+      <a-collapse-panel header="This is panel header 1" key="1">
+        <p>{{text}}</p>
+      </a-collapse-panel>
+      <a-collapse-panel header="This is panel header 2" key="2" :disabled="false">
+        <p>{{text}}</p>
+      </a-collapse-panel>
+      <a-collapse-panel header="This is panel header 3" key="3" disabled>
+        <p>{{text}}</p>
+      </a-collapse-panel>
+    </a-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      text: `A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.`,
+      activeKey: ['1'],
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  watch: {
+    activeKey(key) {
+      console.log(key);
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
